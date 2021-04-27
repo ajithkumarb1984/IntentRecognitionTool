8516245057cd51c1c0b2578926c5670ca01e9aba
Ajith <83013682+ajithkumarb1984@users.noreply.github.com>
2021-04-27 10:48:28 +0200

    Update IntentRecognizer.h
    minor formatting :alignment done

--- include/IntentRecognizer.h
+++ include/IntentRecognizer.h
@@ -11,31 +11,30 @@
 
 using namespace std;
 
-
 class IntentRecognizer
 {
 public:
 	IntentRecognizer();
 	~IntentRecognizer();
 	/*main routine to calculate intent*/
-    string GetIntent(const int i_countWords, char** i_words);
-    string GetIntent(const string i_sentence);
+    	string GetIntent(const int i_countWords, char** i_words);
+    	string GetIntent(const string i_sentence);
 
 private:
-    /* Member variables for extracting and storing Keywords */
-    string m_Weather;
-    string m_Fact;
-    string m_Time;
-    string m_Date;
-    string m_Place;
+    	/* Member variables for extracting and storing Keywords */
+	 string m_Weather;
+	 string m_Fact;
+	 string m_Time;
+	 string m_Date;
+	 string m_Place;
 	
 	/*data parsing and string utilities*/	
-    string Convert2DCharArrayToString(const int i_count, char** i_array);
-    size_t findCaseInsensitive(std::string data, std::string toSearch, size_t pos);
+        string Convert2DCharArrayToString(const int i_count, char** i_array);
+        size_t findCaseInsensitive(std::string data, std::string toSearch, size_t pos);
+	
 	/*intent sub-routines*/
 	void findWeatherIntent(const string i_sentence);
 	void findFactIntent(const string i_sentence);
 	void findCalendarIntent(const string i_sentence);
 };
-
 #endif
